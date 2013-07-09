671402808eefed7d47ba49ff0f35fef19f55967e
Alex Weckiewicz <alexwe@gmail.com>
2013-07-09 15:30:41 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buff = fs.readFileSync('index.html');
+var str = buff.toString('utf-8');
+
+console.log(buff);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+    response.send(str);
 });
 
 var port = process.env.PORT || 5000;
